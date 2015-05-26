6feb173143fe14e54c4f7a5307f767b4ab7b4d62
Sergey Lukin <contact@sergeylukin.com>
2015-05-26 14:41:05 +0300

    Remove author message view

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@ var debounce = require('./modules/debounce');
 // As soon as all elements will be loaded scrolling will be enabled
 scrolling.disable();
 
+document.getElementById('YGLF-intro').classList.remove('is-hidden');
 
 if (Modernizr.svg && viewport.width() > 480 && !device.isMobileOrTablet()) {
 
@@ -34,18 +35,11 @@ if (Modernizr.svg && viewport.width() > 480 && !device.isMobileOrTablet()) {
 }
 
 
-Pace.on('start', function(){
-
-  document.querySelector('.AuthorMessage').classList.remove('is-hidden');
-
-});
-
-
 Pace.on('done', function(){
 
   scrolling.enable();
 
-  document.getElementById('YGLF-intro').classList.remove('is-hidden');
+  document.getElementById('ScrollingHelpfulMessage').classList.remove('is-hidden');
 
   // Remember to perform this after YGLF-intro container is unhidden
   city.update();
